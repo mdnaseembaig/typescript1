1fe98d7b4e8e3cd0b32d1ae9c9461aa3927024a8
mdnaseembaig <mdnaseembit@gmail.com>
2024-09-03 23:56:07 +0530

    typescript

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+let sales: number = 123_456_789;
+let course: string = 'Typescript';
+let is_published = true;
+let level;
+
+
+function render(document: any) {
+    console.log(document)
+
+}
+
+let number: number[] = [];
+number.forEach(n => n.)
+
+// 1, 'Naseem'
+let user: [number, string, boolean, number] = [1, 'Naseem', true, 0];
+user.push(1);
+
+// const samll = 1;
+// const medium = 2;
+// const large =  3;
+
+// pascalcase
+const enum Size { Samll = 1, Medium,Large };
+let mySize: Size.Medium;
+console.log(mySize);
